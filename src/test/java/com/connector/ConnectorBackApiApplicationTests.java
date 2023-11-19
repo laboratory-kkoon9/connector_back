39c1cd83678fbe78cbbf6aea05195d8d4bc2aa3f
kkoon9 <rndrnjs2003@gmail.com>
2023-11-19 12:39:39 +0900

    project settings

---
+++ src/test/java/com/connector/ConnectorBackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorBackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
