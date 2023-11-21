3ebef1ad170baa3a385c9de79d7a168e8a09f8af
kkoon9 <rndrnjs2003@gmail.com>
2023-11-21 22:13:30 +0900

    fix: repository 분리
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/connector/service/ProfileService.java
+++ src/main/java/com/connector/service/ProfileService.java
@@ -1,6 +1,7 @@
 package com.connector.service;
 
 import com.connector.domain.Profile;
+import com.connector.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,26 +12,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
-    private List<Profile> profiles = Arrays.asList(Profile.builder()
-                    .id(1L)
-                    .name("Nam Koong Kwon")
-                    .avatar("https://avatars.githubusercontent.com/u/43670900?s=96&v=4")
-                    .company("student at SK Hynix")
-                    .location("Bucheon")
-                    .skills(Arrays.asList("React", "Expess", "Spring Framework"))
-                    .build(),
-
-            Profile.builder()
-                    .id(2L)
-                    .name("Choi Jin Min")
-                    .avatar("")
-                    .company("Developer at asdf")
-                    .location("asdf")
-                    .skills(Arrays.asList("Android"))
-                    .build()
-    );
+    private final ProfileRepository profileRepository;
 
     public List<Profile> getProfiles() {
-        return profiles;
+        return profileRepository.findAll();
     }
 }
