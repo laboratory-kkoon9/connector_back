36c41c3036c3c16b285bc377579da71d3a45b381
kkoon9 <rndrnjs2003@gmail.com>
2023-11-25 00:23:17 +0900

    fix: ProfileRepository 하드코딩 제거

--- src/main/java/com/connector/repository/ProfileRepository.java
+++ src/main/java/com/connector/repository/ProfileRepository.java
@@ -3,6 +3,7 @@ package com.connector.repository;
 import com.connector.domain.Education;
 import com.connector.domain.Experience;
 import com.connector.domain.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,57 +12,5 @@ import java.util.Collections;
 import java.util.List;
 
 @Repository
-public class ProfileRepository {
-    private List<Profile> profiles = Arrays.asList(Profile.builder()
-                    .id(1L)
-                    .name("Nam Koong Kwon")
-                    .avatar("https://avatars.githubusercontent.com/u/43670900?s=96&v=4")
-                    .company("student at SK Hynix")
-                    .location("Bucheon")
-                    .skills(Arrays.asList("React", "Expess", "Spring Framework"))
-                    .education(Arrays.asList(Education.builder()
-                                    .school("덕산중학교")
-                                    .degree(3)
-                                    .fieldofstudy("공과대")
-                                    .from(LocalDate.of(2020, 1, 1))
-                                    .to(LocalDate.of(2023, 1, 1))
-                                    .build(), Education.builder()
-                                    .school("덕산고등학교")
-                                    .degree(3)
-                                    .fieldofstudy("이대")
-                                    .from(LocalDate.of(2011, 1, 1))
-                                    .to(LocalDate.of(2014, 2, 28))
-                                    .build()
-                            )
-                    )
-                    .experience(Arrays.asList(
-                            Experience.builder()
-                                    .company("SK")
-                                    .position("Junior Developer")
-                                    .description("JSK")
-                                    .from(LocalDate.of(2011, 1, 1))
-                                    .build()
-                    ))
-                    .build(),
-
-            Profile.builder()
-                    .id(2L)
-                    .name("Choi Jin Min")
-                    .avatar("")
-                    .company("Developer at asdf")
-                    .location("asdf")
-                    .skills(Arrays.asList("Android"))
-                    .education(Collections.emptyList())
-                    .experience(Collections.emptyList())
-                    .build()
-    );
-
-
-    public List<Profile> findAll() {
-        return profiles;
-    }
-
-    public Profile findById(Long userId) {
-        return profiles.get(userId.intValue() - 1);
-    }
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
