e1ba8ccd69ca54abc63c28fbba94155a046e197a
kkoon9 <rndrnjs2003@gmail.com>
2023-11-26 19:57:05 +0900

    fix: 사용하지 않는 import 제거

--- src/main/java/com/connector/repository/ProfileRepository.java
+++ src/main/java/com/connector/repository/ProfileRepository.java
@@ -1,15 +1,9 @@
 package com.connector.repository;
 
-import com.connector.domain.Education;
-import com.connector.domain.Experience;
 import com.connector.domain.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
