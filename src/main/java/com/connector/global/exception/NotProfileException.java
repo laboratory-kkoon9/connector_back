debf003d17067b96a12460c802129eab01945f03
kkoon9 <rndrnjs2003@gmail.com>
2023-12-03 16:50:06 +0900

    feat: exception 추가

---
+++ src/main/java/com/connector/global/exception/NotProfileException.java
@@ -0,0 +1,7 @@
+package com.connector.global.exception;
+
+public class NotProfileException extends RuntimeException {
+    public NotProfileException() {
+        super("Not Profile");
+    }
+}
---
+++ src/main/java/com/connector/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.connector.repository;
+
+import com.connector.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
