106e074c215dd34d7e5fc7cf6a0223fa5319f5c8
jaehyuuk <doogun97@gmail.com>
2023-12-08 23:24:31 +0900

    refactor: 필요한 값만 추출하기 위해 Controller, Service 리팩토링

--- src/main/java/com/connector/service/ProfileService.java
+++ src/main/java/com/connector/service/ProfileService.java
@@ -1,12 +1,15 @@
 package com.connector.service;
 
 import com.connector.domain.Profile;
+import com.connector.domain.Skill;
+import com.connector.dto.ProfileDto;
 import com.connector.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,7 +18,16 @@ public class ProfileService {
 
     private final ProfileRepository profileRepository;
 
-    public List<Profile> getProfiles() {
-        return profileRepository.findAll();
+    public List<ProfileDto> getProfiles() {
+        List<Profile> profiles = profileRepository.findAll();
+        List<ProfileDto> profileDtos = profiles.stream()
+                .map(p -> new ProfileDto(
+                        p.getUser(),
+                        p.getExperiences().isEmpty() ? "" :  p.getExperiences().get(0).getCompany(),
+                        p.getLocation(),
+                        p.getBio(),
+                        p.getSkills().stream().map(Skill::getName).collect(Collectors.toList())))
+                .collect(Collectors.toList());
+        return profileDtos;
     }
 }
