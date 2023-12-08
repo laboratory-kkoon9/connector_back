f749b3832cdd36ecab871134afc858bb806a873c
jaehyuuk <doogun97@gmail.com>
2023-12-08 23:24:31 +0900

    refactor: 필요한 값만 추출하기 위해 Controller, Service 리팩토링

--- src/main/java/com/connector/controller/ProfileController.java
+++ src/main/java/com/connector/controller/ProfileController.java
@@ -1,12 +1,13 @@
 package com.connector.controller;
 
-import com.connector.domain.Profile;
+import com.connector.dto.ProfileDto;
 import com.connector.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class ProfileController {
@@ -14,7 +15,7 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @GetMapping("/api/profile")
-    public List<Profile> profileList() {
+    public List<ProfileDto> profileList() {
         return profileService.getProfiles();
     }
 
