0d8ac20b5d186b66be937ed70d7c985c537b090e
jaehyuuk <doogun97@gmail.com>
2023-12-10 16:36:46 +0900

    refactor: Experience, Education 관련 Dto, Service 코드 리팩토링

--- src/main/java/com/connector/dto/EducationDto.java
+++ src/main/java/com/connector/dto/EducationDto.java
@@ -1,16 +1,23 @@
 package com.connector.dto;
 
-import lombok.AllArgsConstructor;
+import com.connector.domain.Education;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
-@AllArgsConstructor
 public class EducationDto {
     private String school;
     private Integer degree;
     private String fieldOfStudy;
     private LocalDate from;
     private LocalDate to;
+
+    public EducationDto(Education education) {
+        school = education.getSchool();
+        degree = education.getDegree();
+        fieldOfStudy = education.getFieldOfStudy();
+        from = education.getStartDate();
+        to = education.getEndDate();
+    }
 }
--- src/main/java/com/connector/service/ProfileService.java
+++ src/main/java/com/connector/service/ProfileService.java
@@ -23,12 +23,12 @@ public class ProfileService {
     public List<ProfileDto> getProfileList() {
         List<Profile> profiles = profileRepository.findAll();
         List<ProfileDto> profileDtos = profiles.stream()
-                .map(p -> new ProfileDto(
-                        p.getUser(),
-                        p.getExperiences().isEmpty() ? "" : p.getExperiences().get(0).getCompany(),
-                        p.getLocation(),
-                        p.getBio(),
-                        p.getSkills().stream().map(Skill::getName).collect(Collectors.toList())))
+                .map(profile -> new ProfileDto(
+                        profile.getUser(),
+                        profile.getExperiences().isEmpty() ? "" : profile.getExperiences().get(0).getCompany(),
+                        profile.getLocation(),
+                        profile.getBio(),
+                        profile.getSkills().stream().map(Skill::getName).collect(Collectors.toList())))
                 .collect(Collectors.toList());
         return profileDtos;
     }
@@ -41,20 +41,9 @@ public class ProfileService {
                 profile.getLocation(),
                 profile.getBio(),
                 profile.getSkills().stream().map(Skill::getName).collect(Collectors.toList()),
-                profile.getExperiences().stream().map(ex -> new ExperienceDto(
-                        ex.getCompany(),
-                        ex.getPosition(),
-                        ex.getDescription(),
-                        ex.getStartDate(),
-                        ex.getEndDate())).collect(Collectors.toList()),
-                profile.getEducations().stream().map(ed -> new EducationDto(
-                        ed.getSchool(),
-                        ed.getDegree(),
-                        ed.getFieldOfStudy(),
-                        ed.getStartDate(),
-                        ed.getEndDate())).collect(Collectors.toList())
+                profile.getExperiences().stream().map(ExperienceDto::new).collect(Collectors.toList()),
+                profile.getEducations().stream().map(EducationDto::new).collect(Collectors.toList())
         );
         return profileDetailDto;
     }
-
 }
