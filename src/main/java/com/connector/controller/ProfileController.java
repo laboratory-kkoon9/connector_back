4a2b20d7771d799afdab427cfbcebf35756e2b2b
jaehyuuk <doogun97@gmail.com>
2023-12-10 17:25:33 +0900

    feat: RequestMapping 추가

--- src/main/java/com/connector/controller/ProfileController.java
+++ src/main/java/com/connector/controller/ProfileController.java
@@ -6,22 +6,24 @@ import com.connector.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/profile")
 @RequiredArgsConstructor
 public class ProfileController {
 
     private final ProfileService profileService;
 
-    @GetMapping("/api/profile")
+    @GetMapping
     public List<ProfileDto> profileList() {
         return profileService.getProfileList();
     }
 
-    @GetMapping("/api/profile/user/{userId}")
+    @GetMapping("/user/{userId}")
     public ProfileDetailDto profileDetail(@PathVariable("userId") Long userId) {
         return profileService.getProfileDetail(userId);
     }
