4ee7c13a944c3e69ff4367f08a0028fd136a0d62
jaehyuuk <doogun97@gmail.com>
2023-12-10 16:36:46 +0900

    refactor: Experience, Education 관련 Dto, Service 코드 리팩토링

--- src/main/java/com/connector/dto/ExperienceDto.java
+++ src/main/java/com/connector/dto/ExperienceDto.java
@@ -1,16 +1,23 @@
 package com.connector.dto;
 
-import lombok.AllArgsConstructor;
+import com.connector.domain.Experience;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
-@AllArgsConstructor
 public class ExperienceDto {
     private String company;
     private String position;
     private String description;
     private LocalDate from;
     private LocalDate to;
+
+    public ExperienceDto(Experience experience) {
+        company = experience.getCompany();
+        position = experience.getPosition();
+        description = experience.getDescription();
+        from = experience.getStartDate();
+        to = experience.getEndDate();
+    }
 }
