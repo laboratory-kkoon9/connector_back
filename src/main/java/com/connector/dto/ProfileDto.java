7d98f1df826d4008c4bff34579adba1de967a3fd
jaehyuuk <doogun97@gmail.com>
2023-12-10 17:11:03 +0900

    refactor: Service에서 실행되는 로직을 Dto에서 수행하도록 리팩토링

--- src/main/java/com/connector/dto/ProfileDto.java
+++ src/main/java/com/connector/dto/ProfileDto.java
@@ -1,17 +1,26 @@
 package com.connector.dto;
 
+import com.connector.domain.Profile;
+import com.connector.domain.Skill;
 import com.connector.domain.User;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class ProfileDto {
     private User user;
     private String company;
     private String location;
     private String bio;
     private List<String> skills;
+
+    public ProfileDto(Profile profile) {
+        user = profile.getUser();
+        company = profile.getExperiences().isEmpty() ? "" : profile.getExperiences().get(0).getCompany();
+        location = profile.getLocation();
+        bio = profile.getBio();
+        skills = profile.getSkills().stream().map(Skill::getName).collect(Collectors.toList());
+    }
 }
