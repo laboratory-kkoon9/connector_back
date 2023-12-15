10893d83fe5e06661d1b77d739b747287a98aca5
jaehyuuk <doogun97@gmail.com>
2023-12-15 18:55:27 +0900

    feat: 회원가입시 email validation 체크 기능 추가

--- src/main/java/com/connector/repository/UserRepository.java
+++ src/main/java/com/connector/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.connector.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Boolean existsByEmail(String email);
 }
--- src/main/java/com/connector/service/UserService.java
+++ src/main/java/com/connector/service/UserService.java
@@ -1,6 +1,7 @@
 package com.connector.service;
 
 import com.connector.dto.RegisterDto;
+import com.connector.global.exception.DuplicateUserEmailException;
 import com.connector.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ public class UserService {
     private final UserRepository userRepository;
 
     public void register(RegisterDto registerDto) {
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new DuplicateUserEmailException();
+        }
         userRepository.save(registerDto.toEntity());
     }
 }
