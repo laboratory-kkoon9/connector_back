b0e690a615ed6eb7b0fc2caa0c0959dff2d38e96
jaehyuuk <doogun97@gmail.com>
2023-12-17 12:56:47 +0900

    feat: 회원가입시 token값 생성

--- src/main/java/com/connector/service/UserService.java
+++ src/main/java/com/connector/service/UserService.java
@@ -1,7 +1,10 @@
 package com.connector.service;
 
+import com.connector.domain.User;
 import com.connector.dto.RegisterDto;
+import com.connector.dto.TokenDto;
 import com.connector.global.exception.DuplicateUserEmailException;
+import com.connector.global.token.TokenManager;
 import com.connector.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +14,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TokenManager tokenManager;
 
     public void register(RegisterDto registerDto) {
         if (userRepository.existsByEmail(registerDto.getEmail())) {
             throw new DuplicateUserEmailException();
         }
-        userRepository.save(registerDto.toEntity());
+        User user = userRepository.save(registerDto.toEntity());
+
+        TokenDto tokenDto = TokenDto.builder().userId(user.getId()).build();
+
+        tokenManager.generateToken(tokenDto);
     }
 }
