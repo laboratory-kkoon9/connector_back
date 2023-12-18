e730bcacd6267cd1af76720ebc62837c1bad04f0
jaehyuuk <doogun97@gmail.com>
2023-12-18 14:46:42 +0900

    feat: Token값 전달을 위한 기능 추가

--- src/main/java/com/connector/controller/AuthController.java
+++ src/main/java/com/connector/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.connector.controller;
 
 import com.connector.dto.LoginDto;
+import com.connector.dto.TokenResponseDto;
 import com.connector.dto.UserDto;
 import com.connector.global.context.TokenContext;
 import com.connector.global.context.TokenContextHolder;
@@ -23,7 +24,7 @@ public class AuthController {
     }
 
     @PostMapping
-    public String login(@RequestBody LoginDto loginDto) {
-        return userService.login(loginDto);
+    public TokenResponseDto login(@RequestBody LoginDto loginDto) {
+        return new TokenResponseDto(userService.login(loginDto));
     }
 }
--- src/main/java/com/connector/dto/TokenResponseDto.java
+++ src/main/java/com/connector/dto/TokenResponseDto.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class RegisterResponseDto {
+public class TokenResponseDto {
     private String token;
 }
