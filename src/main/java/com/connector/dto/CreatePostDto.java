b33f52b775bd0979ce5cbbc605e10fc1817d8d1d
jaehyuuk <doogun97@gmail.com>
2024-01-03 22:02:23 +0900

    feat: 게시물 작성을 위한 api 추가

---
+++ src/main/java/com/connector/dto/CreatePostDto.java
@@ -0,0 +1,22 @@
+package com.connector.dto;
+
+import com.connector.domain.Post;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Getter
+public class CreatePostDto {
+    private String text;
+
+    public Post toEntity(Long userId) {
+        return new Post().builder()
+                .userId(userId)
+                .content(text)
+                .createdAt(LocalDateTime.now())
+                .likes(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+    }
+}
