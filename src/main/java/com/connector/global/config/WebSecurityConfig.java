a06d9a821fc5d6a2751e4cfc411aa951a9371fbd
kkoon9 <rndrnjs2003@gmail.com>
2024-01-04 23:30:54 +0900

    feat: filter exception 처리 추가

--- src/main/java/com/connector/global/config/WebSecurityConfig.java
+++ src/main/java/com/connector/global/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
 
 @EnableWebSecurity
@@ -17,6 +18,7 @@ import org.springframework.web.cors.CorsUtils;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenFilter jwtTokenFilter;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -47,6 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .antMatcher("/**")
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtTokenFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
     }
 }
--- src/main/java/com/connector/global/filter/ExceptionHandlerFilter.java
+++ src/main/java/com/connector/global/filter/ExceptionHandlerFilter.java
@@ -3,8 +3,11 @@ package com.connector.global.filter;
 import com.connector.global.exception.UsernameFromTokenException;
 import com.connector.global.exception.handler.ErrorDetailResponse;
 import com.connector.global.exception.handler.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,10 +16,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -35,11 +41,20 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     }
 
-    public void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable ex) {
+    private void setErrorResponse(HttpStatus status, HttpServletResponse response, Throwable ex) {
         response.setStatus(status.value());
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        ErrorResponse errorResponse = new ErrorResponse(new ErrorDetailResponse(ex.getMessage()));
+        ErrorDetailResponse detailResponse = ErrorDetailResponse.builder().msg(ex.getMessage()).build();
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errors(Arrays.asList(detailResponse))
+                .build();
+
+        try{
+            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
