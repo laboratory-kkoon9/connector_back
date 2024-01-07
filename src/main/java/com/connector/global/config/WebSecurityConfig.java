b9a447ca2b27f85812af994dd31bcfafcf4ba1ee
kkoon9 <rndrnjs2003@gmail.com>
2024-01-07 22:23:52 +0900

    fix: UsernamePasswordAuthenticationFilter 삭제

--- src/main/java/com/connector/global/config/WebSecurityConfig.java
+++ src/main/java/com/connector/global/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
 
@@ -41,6 +40,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .antMatcher("/**")
                 .addFilterBefore(jwtTokenFilter, BasicAuthenticationFilter.class)
-                .addFilterBefore(exceptionHandlerFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
     }
 }
