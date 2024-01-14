e3165fcfe7ffb86a441d61b3370e8d64779e1505
wkdckd41 <wkdckd41@gmail.com>
2024-01-15 03:07:01 +0900

    comment: 모든프로필조회 API (ProfileDto, UserDto)

--- src/main/java/com/connector/dto/ProfileDto.java
+++ src/main/java/com/connector/dto/ProfileDto.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자 자동생성
 public class ProfileDto {
     private UserDto user;
     private String company;
--- src/main/java/com/connector/dto/UserDto.java
+++ src/main/java/com/connector/dto/UserDto.java
@@ -7,8 +7,8 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // 기본생성자
+@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자 자동생성
 public class UserDto {
     private Long id;
     private String name;
