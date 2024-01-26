aa63af3e6c79822e7c4bffd8ad68826067d4e886
kkoon9 <rndrnjs2003@gmail.com>
2024-01-26 16:49:01 +0900

    refactor: WebSecurityConfigurerAdapter deprecated 변경

--- src/main/java/com/connector/global/config/WebSecurityConfig.java
+++ src/main/java/com/connector/global/config/WebSecurityConfig.java
@@ -3,27 +3,26 @@ package com.connector.global.config;
 import com.connector.global.filter.ExceptionHandlerFilter;
 import com.connector.global.filter.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsUtils;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Order(300)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class WebSecurityConfig {
     private final JwtTokenFilter jwtTokenFilter;
     private final ExceptionHandlerFilter exceptionHandlerFilter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable().cors()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors()
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
@@ -32,24 +31,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/api/**").permitAll()
                 .antMatchers(
                         "/",
-                        "/ping",
-                        "/profile",
-                        "/webTest",
-                        "/active",
-                        "/favicon.ico",
-                        "/common/**",
-                        "/open-api/**",
-                        "/actuator/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
                         "/swagger-ui.html")
                 .permitAll()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .antMatcher("/**")
                 .addFilterBefore(jwtTokenFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(exceptionHandlerFilter, JwtTokenFilter.class);
+
+        return http.build();
     }
 }
